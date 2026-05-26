0d8f13edfdd6e717cfba8a2c1cd59e221109f77d
Kunal Kumar <kunal.k.engineer@gmail.com>
2026-05-26 23:58:52 +0530

    feat(core): initialize spring core module

---
+++ spring-core/src/main/java/com/kunal/backendlab/springcore/PaymentService.java
@@ -0,0 +1,10 @@
+package com.kunal.backendlab.springcore;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class PaymentService {
+    public void pay(){
+        System.out.println("Paying...");
+    }
+}
---
+++ spring-core/src/main/java/com/kunal/backendlab/springcore/SpringCoreModuleApplication.java
@@ -0,0 +1,21 @@
+package com.kunal.backendlab.springcore;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCoreModuleApplication implements CommandLineRunner {
+@Autowired
+PaymentService paymentService;
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCoreModuleApplication.class, args);
+	}
+
+    @Override
+    public void run(String... args) throws Exception {
+        paymentService.pay();
+    }
+}
